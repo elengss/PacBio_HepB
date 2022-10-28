caa5ed4e4a346ac8e66ed600155f3edaeb89b981
pbalfeuk <52576859+pbalfeuk@users.noreply.github.com>
2022-10-28 14:50:48 +0100

    Update align.sh

--- align_and_quantify/align.sh
+++ align_and_quantify/align.sh
@@ -14,9 +14,13 @@ echo " "
 echo "EXTRACTING HBV DATA FROM BAM FILES FOR R"
 echo "These files are the input for the quantify_viral.R script."
 ### prepare files for R
+### File for recording line numbers in each file (# sequences is 1/4 of number of lines)
+echo "Length   Filename" > linenumbers
+
 for tmp in 1002 1003 1004 1009 1010 1012 1013 1015; do
 echo processing "$tmp"
 samtools view "$tmp"_D3L.bam -o "$tmp"_D3L.noheader.sam
+wc -l "$tmp"_D3L.noheader.sam >> linenumbers
 awk '{print $6}' "$tmp"_D3L.noheader.sam > cigar
 awk '{print $5}' "$tmp"_D3L.noheader.sam > mapq
 awk '{print $4}' "$tmp"_D3L.noheader.sam > pos
@@ -26,6 +30,9 @@ paste query mapq bit pos cigar > "$tmp".D3L.short; done
 echo 'R files generated'
 ### tidy up files
 rm query bit pos mapq cigar
+echo " "
+echo "Files processed, number of lines in each file are :"
+cat linenumbers
 echo " "
 date +”%H:%M:%S”
 echo " "
