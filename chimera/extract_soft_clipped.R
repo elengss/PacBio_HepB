ad3c2a9c695b967b26dc69430eef73d58f4155dc
pbalfeuk <52576859+pbalfeuk@users.noreply.github.com>
2022-11-10 23:08:53 +0000

    Update extract_soft_clipped.R

--- chimera/extract_soft_clipped.R
+++ chimera/extract_soft_clipped.R
@@ -1,10 +1,17 @@
+###
+### R Script to extract the 5' and 3' regions adjacent to HBV sequences.
+### for subqsequent chimera mapping.
+###
+library(Rsamtools)
 
+### function extractsoftclip
 extractsoftclip<-function(inbam,outfq){
 aln <- scanBam(inbam)
+print(outfq)
 aln[[1]]$cigar -> cigar
 cigarRangesAlongQuerySpace(cigar, ops="S") -> cigarir
 
-#### separate the pos and negative strand and 5' 3' end
+### separate the pos and negative strand and 5' and 3' end
 aln[[1]]$cigar[which(aln[[1]]$strand=="+")]->positivecigar
 aln[[1]]$qname[which(aln[[1]]$strand=="+")]->positiveqname
 names(positivecigar)<-positiveqname
@@ -14,7 +21,6 @@ endclip<-list()
 for(i in 1:length(cigarir)){
 cigarir[[i]][which(end(cigarir[[i]])==aln[[1]]$qwidth[i])]->endclip[[i]]
 cigarir[[i]][which(start(cigarir[[i]])==1)]->startclip[[i]]
-print(i)
 }
 
 ##### do positive start
@@ -22,13 +28,11 @@ aln[[1]]$seq[which(aln[[1]]$strand=="+")]->positiveseq
 mylist<-list()
 for(i in 1:length(startclip)){
 extractAt(positiveseq[i],startclip[[i]])->mylist[[i]]
-print(i)
 }
 
 test2<-vector()
 for(i in 1:length(mylist)){
 length(mylist[[i]][[1]])->test2[i]
-print(i)
 }
 
 aln[[1]]$qname[which(aln[[1]]$strand=="+")]->positiveqname
@@ -36,30 +40,29 @@ positiveqname[which(test2>0)]-> positiveqname2
 mylist[which(test2>0)]->mylist2
 LargeDNAStringSet <- DNAStringSet(sapply(sapply(mylist2, `[[`, 1),"[[",1))
 positiveqname2->names(LargeDNAStringSet)
-writeXStringSet(LargeDNAStringSet, paste(outfq,".positive.start.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
+print("Positive start file generated")
+writeXStringSet(LargeDNAStringSet, paste("data/",outfq,".positive.start.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
 
 ### do positive end
 aln[[1]]$seq[which(aln[[1]]$strand=="+")]->positiveseq
 mylist<-list()
 for(i in 1:length(endclip)){
 extractAt(positiveseq[i],endclip[[i]])->mylist[[i]]
-print(i)
 }
 
 test2<-vector()
 for(i in 1:length(mylist)){
 length(mylist[[i]][[1]])->test2[i]
-print(i)
 }
 aln[[1]]$qname[which(aln[[1]]$strand=="+")]->positiveqname
 positiveqname[which(test2>0)]-> positiveqname2
 mylist[which(test2>0)]->mylist2
 LargeDNAStringSet <- DNAStringSet(sapply(sapply(mylist2, `[[`, 1),"[[",1))
 positiveqname2->names(LargeDNAStringSet)
-writeXStringSet(LargeDNAStringSet, paste(outfq,".positive.end.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
+print("Positive end file generated")
+writeXStringSet(LargeDNAStringSet, paste("data/",outfq,".positive.end.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
 
 #### do negative start
-
 aln[[1]]$cigar[which(aln[[1]]$strand=="-")]->negativecigar
 aln[[1]]$qname[which(aln[[1]]$strand=="-")]->negativeqname
 names(negativecigar)<-negativeqname
@@ -69,42 +72,58 @@ endclip<-list()
 for(i in 1:length(cigarir)){
 cigarir[[i]][which(end(cigarir[[i]])==aln[[1]]$qwidth[i])]->endclip[[i]]
 cigarir[[i]][which(start(cigarir[[i]])==1)]->startclip[[i]]
-print(i)
 }
 aln[[1]]$seq[which(aln[[1]]$strand=="-")]->negativeseq
 mylist<-list()
 for(i in 1:length(startclip)){
 extractAt(negativeseq[i],startclip[[i]])->mylist[[i]]
-print(i)
 }
 test2<-vector()
 for(i in 1:length(mylist)){
 length(mylist[[i]][[1]])->test2[i]
-print(i)
 }
 aln[[1]]$qname[which(aln[[1]]$strand=="-")]->negativeqname
 negativeqname[which(test2>0)]-> negativeqname2
 mylist[which(test2>0)]->mylist2
 LargeDNAStringSet <- DNAStringSet(sapply(sapply(mylist2, `[[`, 1),"[[",1))
 negativeqname2->names(LargeDNAStringSet)
-writeXStringSet(LargeDNAStringSet, paste(outfq,".negative.start.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
-
+print("Negative start file generated")
+writeXStringSet(LargeDNAStringSet, paste("data/",outfq,".negative.start.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")
 
 ### do negative end
 aln[[1]]$seq[which(aln[[1]]$strand=="-")]->negativeseq
 mylist<-list()
 for(i in 1:length(endclip)){
 extractAt(negativeseq[i],endclip[[i]])->mylist[[i]]
-print(i)
 }
 test2<-vector()
 for(i in 1:length(mylist)){
 length(mylist[[i]][[1]])->test2[i]
-print(i)
 }
 aln[[1]]$qname[which(aln[[1]]$strand=="-")]->negativeqname
 negativeqname[which(test2>0)]-> negativeqname2
 mylist[which(test2>0)]->mylist2
 LargeDNAStringSet <- DNAStringSet(sapply(sapply(mylist2, `[[`, 1),"[[",1))
 negativeqname2->names(LargeDNAStringSet)
-writeXStringSet(LargeDNAStringSet, paste(outfq,".negative.end.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")}
+print("Negative end file generated")
+writeXStringSet(LargeDNAStringSet, paste("data/",outfq,".negative.end.fastq",sep=""), append=FALSE,compress=FALSE, compression_level=NA, format="fastq")}
+### end of function extractsoftclip
+###
+### read bam fie names and generate calls to function.
+###
+print("Soft clip utility")
+print(" ")
+print("WARNING! This script is SLOW")
+print("Probably best to start it off at the end of the day and collect in the morning...")
+###
+### read in the file barcodes
+c("1002","1003","1004","1009","1010","1012","1013","1015")->all
+###
+### read bam file names and generate calls to function.
+for(i in 1:length(all)){
+  ### read in the bam files generated by the shell script "align-script.sh"
+  Bamname <- paste("data/",all[i],"_D3L.bam",sep="")
+  print(Bamname)
+  fqname <- all[i]
+  extractsoftclip(Bamname,fqname)
+} 
