fe1db0198362a8c840e3d6ba9e908326f9a805bb
pbalfeuk <52576859+pbalfeuk@users.noreply.github.com>
2022-11-14 12:52:33 +0000

    Update quantify_human.R

--- align_and_quantify/quantify_human.R
+++ align_and_quantify/quantify_human.R
@@ -5,6 +5,6 @@ listmycounts<-list()
 c("1002","1003","1004","1009","1010","1012","1013","1015")->all
 library(Rsubread)
 for(i in 1:length(all)){
-listmycounts[[i]]<-featureCounts(paste(all[i],"_prec_iso_fq_human.bam",sep=""),annot.ext="/reference/gencode.v40.chr_patch_hapl_scaff.annotation.gtf", isGTFAnnotationFile=TRUE, isPairedEnd=FALSE,allowMultiOverlap=F,GTF.featureType=c("transcript"),GTF.attrType=c("gene_id"),isLongRead=T,useMetaFeatures=T,minMQS=30,countMultiMappingReads=F,reportReads="CORE")
+listmycounts[[i]]<-featureCounts(paste(all[i],"_prec_iso_fq_human.bam",sep=""),annot.ext="reference/gencode.v42.chr_patch_hapl_scaff.annotation.gtf", isGTFAnnotationFile=TRUE, isPairedEnd=FALSE,allowMultiOverlap=F,GTF.featureType=c("transcript"),GTF.attrType=c("gene_id"),isLongRead=T,useMetaFeatures=T,minMQS=30,countMultiMappingReads=F,reportReads="CORE")
 print(i)
 }
