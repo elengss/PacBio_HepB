859ba61c775982a3ee243fd25f04b480b27cd9c5
pbalfeuk <52576859+pbalfeuk@users.noreply.github.com>
2022-11-28 14:43:44 +0000

    Update differential.R

--- Differential_expression/differential.R
+++ Differential_expression/differential.R
@@ -2,10 +2,19 @@
 ##### perform differential analysis on human and viral reads
 ##### 
 ##### Other comparisons just require a minor re-write of the code here
-##### All functions used are in the edgeR package.
+##### All functions used are in the edgeR and Rsubread packages.
 #####
 library(edgeR)
+library(Rsubread)
 ####
+##### map human reads to human transcript gtf file using files generated in "align-script.sh"
+##### produces a list of mapped transcripts for each sample, and also outputs files with suffix featureCounts which describe which reads map to each transcript
+listmycounts<-list()
+c("1002","1003","1004","1009","1010","1012","1013","1015")->all
+for(i in 1:length(all)){
+listmycounts[[i]]<-featureCounts(paste("data/",all[i],"_prec_iso_fq_human.bam",sep=""),annot.ext="reference/gencode.v42.chr_patch_hapl_scaff.annotation.gtf", isGTFAnnotationFile=TRUE, isPairedEnd=FALSE,allowMultiOverlap=F,GTF.featureType=c("transcript"),GTF.attrType=c("gene_id"),isLongRead=T,useMetaFeatures=T,minMQS=30,countMultiMappingReads=F,reportReads="CORE")
+print(i)
+}
 matrix(nrow=nrow(listmycounts[[1]][[1]]),ncol=8)->counts
 for(i in 1:8){
 listmycounts[[i]][[1]]->counts[,i]}
